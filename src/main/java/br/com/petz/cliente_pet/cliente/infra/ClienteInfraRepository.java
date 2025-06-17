e703e18dabd96aaa4aef8da8f31de4b8ce11f9a3
Roberto Pedro <robertopedro100@gmail.com>
2025-06-17 19:49:10 -0300

    Implementa JpaRepository

--- src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
@@ -2,17 +2,22 @@ package br.com.petz.cliente_pet.cliente.infra;
 
 import br.com.petz.cliente_pet.cliente.application.repository.ClienteRepository;
 import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
+
+    private final CienteSpringDataJPARepository cienteSpringDataJPARepository;
 
     @Override
     public Cliente salva(Cliente cliente) {
         log.info("[inicia] ClienteInfraRepository - salva");
-//        cienteSpringDataJPARepository
+        cienteSpringDataJPARepository.save(cliente);
         log.info("[finaliza] ClienteInfraRepository - salva");
         return cliente;
     }
