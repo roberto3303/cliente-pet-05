18bbd4fe053f2f2099e94944c74f35a2cf0d7893
Roberto Pedro <robertopedro100@gmail.com>
2025-07-15 19:36:11 -0300

    Implementa alteracao

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteController.java
@@ -53,6 +53,7 @@ public class ClienteController implements ClienteAPI {
     public void patchAlteraCliente(UUID idCliente, ClienteAlteracaoRequest clienteAlteracaoRequest) {
         log.info("[inicia] ClienteController - patchAlteraCliente ");
         log.info("[idCliente] {}", idCliente);
+        clienteService.patchAlteraCliente(idCliente, clienteAlteracaoRequest);
         log.info("[finaliza] ClienteController - patchAlteraCliente ");
     }
 }
