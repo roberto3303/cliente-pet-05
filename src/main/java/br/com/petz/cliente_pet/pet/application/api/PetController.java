4f8c06f44399199ab0c8f6e83c49607002d15f6b
Roberto Pedro <robertopedro100@gmail.com>
2025-07-26 22:05:23 -0300

    Implementa fluxo service

--- src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/api/PetController.java
@@ -1,5 +1,8 @@
 package br.com.petz.cliente_pet.pet.application.api;
 
+import br.com.petz.cliente_pet.pet.application.service.PetService;
+import br.com.petz.cliente_pet.pet.domain.Pet;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +10,16 @@ import java.util.UUID;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PetController implements PetAPI {
+    private final PetService petService;
+
     @Override
     public PetResponse postPet(UUID idCliente, PetRequest petRequest) {
         log.info("[inicia] PetController - postPet");
         log.info("[idCliente] {}", idCliente);
+        PetResponse pet = petService.criaPet(idCliente,petRequest);
         log.info("[finaliza] PetController - postPet");
-        return null;
+        return pet;
     }
 }
--- src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
@@ -1,4 +1,19 @@
 package br.com.petz.cliente_pet.pet.application.service;
 
-public class PetApplicationService {
+import br.com.petz.cliente_pet.pet.application.api.PetRequest;
+import br.com.petz.cliente_pet.pet.application.api.PetResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Log4j2
+public class PetApplicationService implements PetService {
+    @Override
+    public PetResponse criaPet(UUID idCliente, PetRequest petRequest) {
+        log.info("[inicia] PetApplicationService - criaPet");
+        log.info("[finaliza] PetApplicationService - criaPet");
+        return null;
+    }
 }
