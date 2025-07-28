8229ef2837980a57861641a735fb7e9515d76d6d
Roberto Pedro <robertopedro100@gmail.com>
2025-07-28 18:51:46 -0300

    Implementa fluxo do Repository

--- src/main/java/br/com/petz/cliente_pet/pet/application/service/PetService.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/service/PetService.java
@@ -9,6 +9,5 @@ import java.util.UUID;
 
 public interface PetService {
     PetResponse criaPet(UUID idCliente, PetRequest petRequest);
-
     List<PetClienteListResponse> buscaPetsDoClienteComID(UUID idCliente);
 }
--- src/main/java/br/com/petz/cliente_pet/pet/infra/PetInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/pet/infra/PetInfraRepository.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 @Log4j2
 @RequiredArgsConstructor
@@ -18,5 +21,12 @@ public class PetInfraRepository implements PetRepository {
         petSpringDataJPARepository.save(pet);
         log.info("[finaliza] PetInfraRepository - salvaPet");
         return pet;
+    }
+
+    @Override
+    public List<Pet> buscaPetsDoClienteComID(UUID idCliente) {
+        log.info("[inicia] PetInfraRepository - buscaPetsDoClienteComID");
+        log.info("[finaliza] PetInfraRepository - buscaPetsDoClienteComID");
+        return List.of();
     }
 }
