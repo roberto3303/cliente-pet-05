19747160c572c83f4c218b98602c229e3019c1f5
Roberto Pedro <robertopedro100@gmail.com>
2025-08-13 20:42:44 -0300

    Implementa Delete-Pet

--- src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/pet/application/service/PetApplicationService.java
@@ -51,6 +51,9 @@ public class PetApplicationService implements PetService {
     @Override
     public void deletePetDoClienteComId(UUID idCliente, UUID idPet) {
         log.info("[inicia] PetApplicationService - deletePetDoClienteComId");
+        clienteService.buscaClienteAtravesId(idCliente);
+        Pet pet = petRepository.buscaPetPeloId(idPet);
+        petRepository.deletaPet(pet);
         log.info("[finaliza] PetApplicationService - deletePetDoClienteComId");
     }
 }
